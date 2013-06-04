20a39870617819cf7a23ba0e95fd6736d6ea5c54
Alex Wolfe <thewolfe@gmail.com>
2013-06-03 23:59:07 -0700

    Move options and glow to seperate file, updated demo page, updated colors

--- js/button.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    setInterval(function(){
-        var buttons = $('.glow');
-        buttons.toggleClass('glow-on');
-    }, 1500);
-});
--- js/main.js
+++ js/main.js
@@ -1,5 +1,41 @@
 $(document).ready(function(){
-    $('.showcase a').on('click', function(e) {
-        e.preventDefault();
-    });
+
+    //CREATE PAGE METHODS
+    var page = {
+        init: function() {
+            this.buttons = $('#main-nav a');
+
+            this.activateNav();
+            this.disableDemoButtons();
+            console.log('hello');
+        },
+
+        activateNav: function() {
+            this.buttons.click(function(e) {
+                e.preventDefault();
+                var currentButton = $(e.currentTarget);
+                var buttonId = currentButton.attr('href');
+
+                //DESELECT ALL BUTTONS & SELECT CURRRENT ONE
+                this.buttons.parent().removeClass('selected');
+                currentButton.parent().addClass('selected');
+
+                //ANIMATE SCROLL EFFECT
+                $("html, body").animate({
+                    scrollTop: $(buttonId).offset().top - 100
+                }, 'slow');
+
+
+            }.bind(this));
+        },
+
+        disableDemoButtons: function() {
+            $('.showcase a').on('click', function(e) {
+                e.preventDefault();
+            });
+        }
+    };
+
+    //INITIALIZE PAGE
+    page.init();
 });
