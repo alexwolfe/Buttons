a320aa8b446da3a42045ec90a860f4e2b0233a24
Alex Wolfe <thewolfe@gmail.com>
2014-09-06 09:33:37 -0700

    More css styles and auto code generator

--- styleguide/js/main.js
+++ styleguide/js/main.js
@@ -1,41 +1,65 @@
 $(document).ready(function(){
 
-    //CREATE PAGE METHODS
-    var page = {
-        init: function() {
-            this.buttons = $('#main-nav a');
+  var helpers = {
+    htmlEncode: function(value){
+      return $('<div/>').text(value).html();
+    },
 
-            this.activateNav();
-            this.disableDemoButtons();
-        },
+    htmlDecode: function(value){
+      return $('<div/>').html(value).text();
+    }
+  };
 
-        activateNav: function() {
-            var that = this;
 
-            this.buttons.click(function(e) {
-                e.preventDefault();
-                var currentButton = $(e.currentTarget);
-                var buttonId = currentButton.attr('href');
+  var page = {
+    init: function() {
+      this.buttons = $('#main-nav a');
 
-                //DESELECT ALL BUTTONS & SELECT CURRRENT ONE
-                that.buttons.parent().removeClass('selected');
-                currentButton.parent().addClass('selected');
+      this.activateNav();
+      this.disableDemoButtons();
+      this.generateCodeSamples();
+    },
 
-                //ANIMATE SCROLL EFFECT
-                $("html, body").animate({
-                    scrollTop: $(buttonId).offset().top - 100
-                }, 'slow');
+    generateCodeSamples: function() {
+      var self = this;
 
-            });
-        },
+      $('.showcase').each(function(index, element) {
+        var $showcase = $(element);
+        var buttonsHTML = helpers.htmlEncode($showcase.find('.showcase-examples:first').html().trim());
 
-        disableDemoButtons: function() {
-            $('.showcase [href^=#]').on('click', function(e) {
-                e.preventDefault();
-            });
-        }
-    };
+        $showcase.append('<pre class="prettyprint linenums">' + buttonsHTML + '</pre>');
+      });
 
-    //INITIALIZE PAGE
-    page.init();
+      prettyPrint();
+    },
+
+    activateNav: function() {
+      var that = this;
+
+      this.buttons.click(function(e) {
+        e.preventDefault();
+        var currentButton = $(e.currentTarget);
+        var buttonId = currentButton.attr('href');
+
+        //DESELECT ALL BUTTONS & SELECT CURRRENT ONE
+        that.buttons.parent().removeClass('selected');
+        currentButton.parent().addClass('selected');
+
+        //ANIMATE SCROLL EFFECT
+        $("html, body").animate({
+            scrollTop: $(buttonId).offset().top - 100
+        }, 'slow');
+      });
+    },
+
+
+    disableDemoButtons: function() {
+      $('[href^=#]').on('click', function(e) {
+        e.preventDefault();
+      });
+    }
+  };
+
+  //INITIALIZE PAGE
+  page.init();
 });
