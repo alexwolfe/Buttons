6f31bf59b385ff488e3f7be2f0454cc031e06310
Alex Wolfe <thewolfe@gmail.com>
2014-09-13 14:23:50 -0700

    more refactor

--- config.rb
+++ config.rb
@@ -4,7 +4,7 @@
 http_path = "/"
 css_dir = "css"
 sass_dir = "scss"
-images_dir = "img"
+images_dir = "images"
 javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
