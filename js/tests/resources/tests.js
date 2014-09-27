2b909f8a61c4c946a1530f0c5ae392a6c340cc72
Rob Levin <pair+rob@mavenlink.com>
2014-09-27 07:32:33 -0700

    Adds window.global_test_results to saucelabs test

--- js/tests/resources/tests.js
+++ js/tests/resources/tests.js
@@ -1,28 +1,26 @@
-var log = [];
-var testName;
-QUnit.done = function (test_results) {
-  var tests = log.map(function(details){
-    return {
+QUnit.done(function (test_results) {
+  var tests = [];
+  for(var i = 0, len = log.length; i < len; i++) {
+    var details = log[i];
+    tests.push({
       name: details.name,
       result: details.result,
       expected: details.expected,
       actual: details.actual,
       source: details.source
-    }
-  });
+    });
+  }
   test_results.tests = tests;
 
-  // delaying results a bit cause in real-world
-  // scenario you won't get them immediately
-  setTimeout(function () { window.global_test_results = test_results; }, 2000);
-};
+  window.global_test_results = test_results;
+});
 QUnit.testStart(function(testDetails){
-  QUnit.log = function(details){
+  QUnit.log(function(details){
     if (!details.result) {
       details.name = testDetails.name;
       log.push(details);
     }
-  }
+  });
 });
 
 test( "hello test", function() {
