750a2ec9fccf888e7f543a0ec9a94a8a0c4bb857
Rob Levin <pair+rob@mavenlink.com>
2014-11-13 09:11:04 -0800

    master

--- showcase/js/buttons.js
+++ showcase/js/buttons.js
@@ -5,6 +5,7 @@
 *  License: Apache License v2.0
 */
 
+
 // the semi-colon before function invocation is a safety net against concatenated
 // scripts and/or other plugins which may not be closed properly.
 ;(function ( $, window, document, undefined ) {
